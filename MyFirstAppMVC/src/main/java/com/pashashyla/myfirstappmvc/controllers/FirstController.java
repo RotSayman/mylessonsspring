e1e2346f5217a84e0b1b3c65ca9d72c51de3955b
Pasha Shyla <PashaShelo@gmail.com>
2020-11-13 17:30:52 +0300

    Web calculator

--- MyFirstAppMVC/src/main/java/com/pashashyla/myfirstappmvc/controllers/FirstController.java
+++ MyFirstAppMVC/src/main/java/com/pashashyla/myfirstappmvc/controllers/FirstController.java
@@ -1,8 +1,10 @@
 package com.pashashyla.myfirstappmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
@@ -16,5 +18,34 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("operation") String operation, Model model){
+        double result;
+        switch (operation) {
+            case "multiply":
+                result = a * b;
+                break;
+                case "divide":
+                result = a / (double) b;
+                break;
+            case "add":
+                result = a + b;
+                break;
+            case "subtract":
+                result = a - b;
+                break;
+            default:
+                result = 0.0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+
+
+        return "first/calculator";
     }
 }
